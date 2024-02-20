7809bf9291320d7ee5422071fd55decdfe1fcf61
EgorShap <bear.egor10@gmail.com>
2024-02-20 12:57:05 +0200

    change link href to 'statistics'

--- front-end/src/components/dashboard-layout/sidebar/Sidebar.tsx
+++ front-end/src/components/dashboard-layout/sidebar/Sidebar.tsx
@@ -8,7 +8,7 @@ import { MenuItem } from "./MenuItem";
 export function Sidebar() {
     return <aside className="border-r border-r-border h-full bg-sidebar flex flex-col justify-between">
         <div>
-            <Link href='/' draggable={false} className="flex items-center gap-2.5 p-layout border-b border-b-border">
+            <Link href='/i' draggable={false} className="flex items-center gap-2.5 p-layout border-b border-b-border">
                 <GanttChartSquare color={COLORS.primary} size={38} />
                 <span className="text-2xl font-bold relative">React Planner</span>
             </Link>
